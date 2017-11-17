5b02a0e818c24164854b172fea7f7ef635b68761
Sree Theerdha <Sree Theerdha>
2017-11-17 17:36:28 +0530

    First Commit with Cloud and Rain

---
+++ HexCell.cs
@@ -0,0 +1,690 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+
+    public HexCoordinates coordinates;
+
+    public RectTransform uiRect;
+
+    public HexGridChunk chunk;
+
+    public Transform cloudInstance;
+
+    public Transform rainInstance;
+
+    public Color Color
+    {
+        get
+        {
+            return color;
+        }
+        set
+        {
+            if (color == value)
+            {
+                return;
+            }
+            color = value;
+            Refresh();
+        }
+    }
+
+    public int Elevation
+    {
+        get
+        {
+            return elevation;
+        }
+        set
+        {
+            if (elevation == value)
+            {
+                return;
+            }
+            elevation = value;
+            Vector3 position = transform.localPosition;
+            position.y = value * HexMetrics.elevationStep;
+            position.y +=
+                (HexMetrics.SampleNoise(position).y * 2f - 1f) *
+                HexMetrics.elevationPerturbStrength;
+            transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = -position.y;
+            uiRect.localPosition = uiPosition;
+
+            ValidateRivers();
+
+            for (int i = 0; i < roads.Length; i++)
+            {
+                if (roads[i] && GetElevationDifference((HexDirection)i) > 1)
+                {
+                    SetRoad(i, false);
+                }
+            }
+
+            Refresh();
+        }
+    }
+
+    public int WaterLevel
+    {
+        get
+        {
+            return waterLevel;
+        }
+        set
+        {
+            if (waterLevel == value)
+            {
+                return;
+            }
+            waterLevel = value;
+            ValidateRivers();
+            Refresh();
+        }
+    }
+
+    public bool IsUnderwater
+    {
+        get
+        {
+            return waterLevel > elevation;
+        }
+    }
+
+    public bool HasIncomingRiver
+    {
+        get
+        {
+            return hasIncomingRiver;
+        }
+    }
+
+    public bool HasOutgoingRiver
+    {
+        get
+        {
+            return hasOutgoingRiver;
+        }
+    }
+
+    public bool HasRiver
+    {
+        get
+        {
+            return hasIncomingRiver || hasOutgoingRiver;
+        }
+    }
+
+    public bool HasRiverBeginOrEnd
+    {
+        get
+        {
+            return hasIncomingRiver != hasOutgoingRiver;
+        }
+    }
+
+    public HexDirection RiverBeginOrEndDirection
+    {
+        get
+        {
+            return hasIncomingRiver ? incomingRiver : outgoingRiver;
+        }
+    }
+
+    public bool HasRoads
+    {
+        get
+        {
+            for (int i = 0; i < roads.Length; i++)
+            {
+                if (roads[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public HexDirection IncomingRiver
+    {
+        get
+        {
+            return incomingRiver;
+        }
+    }
+
+    public HexDirection OutgoingRiver
+    {
+        get
+        {
+            return outgoingRiver;
+        }
+    }
+
+    public Vector3 Position
+    {
+        get
+        {
+            return transform.localPosition;
+        }
+    }
+
+
+    public float StreamBedY
+    {
+        get
+        {
+            return
+                (elevation + HexMetrics.streamBedElevationOffset) *
+                HexMetrics.elevationStep;
+        }
+    }
+
+    public float RiverSurfaceY
+    {
+        get
+        {
+            return
+                (elevation + HexMetrics.waterElevationOffset) *
+                HexMetrics.elevationStep;
+        }
+    }
+
+    public float WaterSurfaceY
+    {
+        get
+        {
+            return
+                (waterLevel + HexMetrics.waterElevationOffset) *
+                HexMetrics.elevationStep;
+        }
+    }
+
+    public int UrbanLevel
+    {
+        get
+        {
+            return urbanLevel;
+        }
+        set
+        {
+            if (urbanLevel != value)
+            {
+                urbanLevel = value;
+                RefreshSelfOnly();
+            }
+        }
+    }
+
+    public int FarmLevel
+    {
+        get
+        {
+            return farmLevel;
+        }
+        set
+        {
+            if (farmLevel != value)
+            {
+                farmLevel = value;
+                RefreshSelfOnly();
+            }
+        }
+    }
+
+    public int PlantLevel
+    {
+        get
+        {
+            return plantLevel;
+        }
+        set
+        {
+            if (plantLevel != value)
+            {
+                plantLevel = value;
+                RefreshSelfOnly();
+            }
+        }
+    }
+
+    public float ProbabilityRain
+    {
+        get
+        {
+            return PrRain;
+        }
+        set
+        {
+            if (PrRain != value)
+            {
+                PrRain = value;
+                RefreshSelfOnly();
+            }
+        }
+    }
+
+    public float ProbabilityCloud
+    {
+        get
+        {
+            return PrCloud;
+        }
+        set
+        {
+            if (PrCloud != value)
+            {
+                PrCloud = value;
+                RefreshSelfOnly();
+            }
+        }
+    }
+
+
+    public bool IsRaining
+    {
+        get
+        {
+            return isRaining;
+        }
+        set
+        {
+            if (isRaining == true && value == false)
+            {
+                isRaining = value;
+                deleteRain();
+                // TODO
+            }
+            else if(isRaining == false && value == true)
+            {
+                isRaining = value;
+                generateRain();
+                // GENERATE TIME STAMP
+            }
+            // RefreshSelfOnly();
+        }
+    }
+
+    public bool IsCloud
+    {
+        get
+        {
+            return isCloud;
+        }
+        set
+        {
+            if (isCloud == true && value == false)
+            {
+                isCloud = value;
+                deleteCloud();
+                // TODO
+            }
+            else if (isCloud == false && value == true)
+            {
+                isCloud = true;
+                generateCloud();
+                // UPDATE TIME STAMP
+            }
+            // RefreshSelfOnly();
+        }
+    }
+
+    public float RainStartTimeStamp
+    {
+        get
+        {
+            return rainStartTimeStamp;
+        }
+        set
+        {
+            rainStartTimeStamp = value;
+        }
+    }
+
+    public float CloudStartTimeStamp
+    {
+        get
+        {
+            return cloudStartTimeStamp;
+        }
+        set
+        {
+            cloudStartTimeStamp = value;
+        }
+    }
+
+    void deleteCloud() { }
+    void generateCloud() {
+        Transform instance = Instantiate(cloudInstance);
+        Vector3 position = Position;
+        position.y += instance.localScale.y * 0.5f;
+        instance.localPosition = HexMetrics.Perturb(position);
+        instance.localRotation = Quaternion.Euler(0f, 360f * 0.5f, 0f);
+    }
+    void deleteRain() { }
+    void generateRain() {
+        Transform instance = Instantiate(rainInstance);
+        Vector3 position = Position;
+        position.y += instance.localScale.y * 15f;
+        instance.localPosition = HexMetrics.Perturb(position);
+        instance.localRotation = Quaternion.Euler(0f, 360f * 0.5f, 0f);
+        
+    }
+
+    public int RainDurationCycles
+    {
+        set
+        {
+            rainDurationCycles = value;
+        }
+        get
+        {
+            return rainDurationCycles;
+        }
+    }
+
+    public int CloudDurationCycles
+    {
+        set
+        {
+            cloudDurationCycles = value;
+        }
+        get
+        {
+            return cloudDurationCycles;
+        }
+    }
+
+    Color color;
+
+    int elevation = int.MinValue;
+    int waterLevel;
+
+    public float PrRain;
+    public float PrCloud;
+    public float PrDrought;
+    public bool isVacant;
+    bool isRaining;
+    bool isCloud;
+    int rainDurationCycles;
+    int cloudDurationCycles;
+
+    float rainStartTimeStamp;
+    float cloudStartTimeStamp;
+
+    float temperature;  // In celsius
+    float pressure; // In kiloPascals
+    float humidity; // In percentage
+    float CO2Percentage;    // In percentage
+    float precipitation;	// in mm
+
+    float rainDurationFactor = 1000; //max rain duration is for 1000 frames
+    float cloudDurationFactor = 1000;//
+
+    int urbanLevel, farmLevel, plantLevel;
+
+    bool hasIncomingRiver, hasOutgoingRiver;
+    HexDirection incomingRiver, outgoingRiver;
+
+    [SerializeField]
+    HexCell[] neighbors;
+
+    [SerializeField]
+    bool[] roads;
+
+    float CalculateRainProbability()
+    {
+        if (isCloud == true) return 0.8f;
+        else return 0.1f;
+    }
+
+    float CalculateCloudProbability()
+    {
+        float c;
+        c = 0.6f * (Mathf.Sqrt(temperature) * humidity * CO2Percentage) / pressure;
+        float cClamped;
+        cClamped = Mathf.Lerp(0f, 1f, c);
+        return cClamped;
+    }
+
+    void CalculateRainDuration()
+    {
+        rainDurationCycles = (int)(PrRain * rainDurationFactor);
+        return;
+    }
+
+    void CalculateCloudDuration()
+    {
+        cloudDurationCycles = (int)(PrCloud * cloudDurationFactor);
+        if (cloudDurationCycles < rainDurationCycles)
+            cloudDurationCycles = rainDurationCycles;
+        return;
+    }
+
+    public void UpdateProbabilities()
+    {
+        PrRain = CalculateRainProbability();
+        PrCloud = CalculateCloudProbability();
+        CalculateRainDuration();
+        CalculateCloudDuration();
+    }
+
+    void UpdateParams()
+    {
+        /*
+        temperature;  // In celsius
+        pressure; // In kiloPascals
+        humidity; // In percentage
+        CO2Percentage;    // In percentage
+        precipitation;	// in mm
+        dependencies on urbanLevel, farmLevel, plantLevel
+        */
+
+        float temp = UrbanLevel / (Mathf.Sqrt(plantLevel) * Mathf.Sqrt(Mathf.Sqrt(FarmLevel)));
+        temperature = Mathf.Lerp(-10f, 50f, temp);
+
+        float press = UrbanLevel / (Mathf.Sqrt(Mathf.Sqrt(plantLevel)) * Mathf.Sqrt(FarmLevel));
+        pressure = Mathf.Lerp(-0.8f, 1.6f, temp);
+
+        float hum = (Mathf.Sqrt(Mathf.Sqrt(plantLevel))) * (Mathf.Sqrt(Mathf.Sqrt(plantLevel))) / UrbanLevel;
+        humidity = Mathf.Lerp(40f, 90f, hum);
+
+        float prec = (Mathf.Sqrt(Mathf.Sqrt(plantLevel))) * (Mathf.Sqrt(Mathf.Sqrt(plantLevel))) / Mathf.Pow(UrbanLevel, 2);
+        precipitation = Mathf.Lerp(0.1f, 9f, hum);
+
+        float CO2 = Mathf.Pow(UrbanLevel, 3) / (Mathf.Sqrt(plantLevel) * Mathf.Sqrt(plantLevel));
+        CO2Percentage = Mathf.Lerp(0.003f, 0.07f, CO2);
+
+    }
+
+    public void UpdateCell()
+    {
+        UpdateParams();
+        UpdateProbabilities();
+    }
+
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+
+    public HexEdgeType GetEdgeType(HexDirection direction)
+    {
+        return HexMetrics.GetEdgeType(
+            elevation, neighbors[(int)direction].elevation
+        );
+    }
+
+    public HexEdgeType GetEdgeType(HexCell otherCell)
+    {
+        return HexMetrics.GetEdgeType(
+            elevation, otherCell.elevation
+        );
+    }
+
+    public bool HasRiverThroughEdge(HexDirection direction)
+    {
+        return
+            hasIncomingRiver && incomingRiver == direction ||
+            hasOutgoingRiver && outgoingRiver == direction;
+    }
+
+    public void RemoveIncomingRiver()
+    {
+        if (!hasIncomingRiver)
+        {
+            return;
+        }
+        hasIncomingRiver = false;
+        RefreshSelfOnly();
+
+        HexCell neighbor = GetNeighbor(incomingRiver);
+        neighbor.hasOutgoingRiver = false;
+        neighbor.RefreshSelfOnly();
+    }
+
+    public void RemoveOutgoingRiver()
+    {
+        if (!hasOutgoingRiver)
+        {
+            return;
+        }
+        hasOutgoingRiver = false;
+        RefreshSelfOnly();
+
+        HexCell neighbor = GetNeighbor(outgoingRiver);
+        neighbor.hasIncomingRiver = false;
+        neighbor.RefreshSelfOnly();
+    }
+
+    public void RemoveRiver()
+    {
+        RemoveOutgoingRiver();
+        RemoveIncomingRiver();
+    }
+
+    public void SetOutgoingRiver(HexDirection direction)
+    {
+        if (hasOutgoingRiver && outgoingRiver == direction)
+        {
+            return;
+        }
+
+        HexCell neighbor = GetNeighbor(direction);
+        if (!IsValidRiverDestination(neighbor))
+        {
+            return;
+        }
+
+        RemoveOutgoingRiver();
+        if (hasIncomingRiver && incomingRiver == direction)
+        {
+            RemoveIncomingRiver();
+        }
+        hasOutgoingRiver = true;
+        outgoingRiver = direction;
+
+        neighbor.RemoveIncomingRiver();
+        neighbor.hasIncomingRiver = true;
+        neighbor.incomingRiver = direction.Opposite();
+
+        SetRoad((int)direction, false);
+    }
+
+    public bool HasRoadThroughEdge(HexDirection direction)
+    {
+        return roads[(int)direction];
+    }
+
+    public void AddRoad(HexDirection direction)
+    {
+        if (
+            !roads[(int)direction] && !HasRiverThroughEdge(direction) &&
+            GetElevationDifference(direction) <= 1
+        )
+        {
+            SetRoad((int)direction, true);
+        }
+    }
+
+    public void RemoveRoads()
+    {
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            if (roads[i])
+            {
+                SetRoad(i, false);
+            }
+        }
+    }
+
+    public int GetElevationDifference(HexDirection direction)
+    {
+        int difference = elevation - GetNeighbor(direction).elevation;
+        return difference >= 0 ? difference : -difference;
+    }
+
+    bool IsValidRiverDestination(HexCell neighbor)
+    {
+        return neighbor && (
+            elevation >= neighbor.elevation || waterLevel == neighbor.elevation
+        );
+    }
+
+    void ValidateRivers()
+    {
+        if (
+            hasOutgoingRiver &&
+            !IsValidRiverDestination(GetNeighbor(outgoingRiver))
+        )
+        {
+            RemoveOutgoingRiver();
+        }
+        if (
+            hasIncomingRiver &&
+            !GetNeighbor(incomingRiver).IsValidRiverDestination(this)
+        )
+        {
+            RemoveIncomingRiver();
+        }
+    }
+
+    void SetRoad(int index, bool state)
+    {
+        roads[index] = state;
+        neighbors[index].roads[(int)((HexDirection)index).Opposite()] = state;
+        neighbors[index].RefreshSelfOnly();
+        RefreshSelfOnly();
+    }
+
+    void Refresh()
+    {
+        if (chunk)
+        {
+            chunk.Refresh();
+            for (int i = 0; i < neighbors.Length; i++)
+            {
+                HexCell neighbor = neighbors[i];
+                if (neighbor != null && neighbor.chunk != chunk)
+                {
+                    neighbor.chunk.Refresh();
+                }
+            }
+        }
+    }
+
+    void RefreshSelfOnly()
+    {
+        chunk.Refresh();
+    }
+}
---
+++ HexMapEditor.cs
@@ -0,0 +1,337 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour
+{
+
+    public Color[] colors;
+
+    public HexGrid hexGrid;
+
+    int activeElevation;
+    int activeWaterLevel;
+
+    int activeUrbanLevel, activeFarmLevel, activePlantLevel;
+
+    Color activeColor;
+
+    int brushSize;
+
+    bool applyColor;
+    bool applyElevation = true;
+    bool applyWaterLevel = true;
+
+    bool applyUrbanLevel, applyFarmLevel, applyPlantLevel;
+
+    enum OptionalToggle
+    {
+        Ignore, Yes, No
+    }
+
+    OptionalToggle riverMode, roadMode;
+
+    bool isDrag;
+    HexDirection dragDirection;
+    HexCell previousCell;
+
+    public void SelectColor(int index)
+    {
+        applyColor = index >= 0;
+        if (applyColor)
+        {
+            activeColor = colors[index];
+        }
+    }
+
+    public void SetApplyElevation(bool toggle)
+    {
+        applyElevation = toggle;
+    }
+
+    public void SetElevation(float elevation)
+    {
+        activeElevation = (int)elevation;
+    }
+
+    public void SetApplyWaterLevel(bool toggle)
+    {
+        applyWaterLevel = toggle;
+    }
+
+    public void SetWaterLevel(float level)
+    {
+        activeWaterLevel = (int)level;
+    }
+
+    public void SetApplyUrbanLevel(bool toggle)
+    {
+        applyUrbanLevel = toggle;
+    }
+
+    public void SetUrbanLevel(float level)
+    {
+        activeUrbanLevel = (int)level;
+    }
+
+    public void SetApplyFarmLevel(bool toggle)
+    {
+        applyFarmLevel = toggle;
+    }
+
+    public void SetFarmLevel(float level)
+    {
+        activeFarmLevel = (int)level;
+    }
+
+    public void SetApplyPlantLevel(bool toggle)
+    {
+        applyPlantLevel = toggle;
+    }
+
+    public void SetPlantLevel(float level)
+    {
+        activePlantLevel = (int)level;
+    }
+
+    public void SetBrushSize(float size)
+    {
+        brushSize = (int)size;
+    }
+
+    public void SetRiverMode(int mode)
+    {
+        riverMode = (OptionalToggle)mode;
+    }
+
+    public void SetRoadMode(int mode)
+    {
+        roadMode = (OptionalToggle)mode;
+    }
+
+    public void ShowUI(bool visible)
+    {
+        hexGrid.ShowUI(visible);
+    }
+
+    void Awake()
+    {
+        SelectColor(0);
+    }
+
+    void Update()
+    {
+        if (
+            Input.GetMouseButton(0) &&
+            !EventSystem.current.IsPointerOverGameObject()
+        )
+        {
+            HandleInput();
+        }
+        else
+        {
+            previousCell = null;
+        }
+        updateCellsState();
+    }
+
+    void updateCellsState()
+    {
+        HexCell temp;
+        float sample = randomSample();
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                HexCoordinates coord = new HexCoordinates(i, j);
+                temp = hexGrid.GetCell(coord);
+                if (temp.PlantLevel != 0 || temp.FarmLevel != 0 || temp.UrbanLevel != 0)
+                {
+                    temp.UpdateProbabilities();
+                    checkRandomEvents(temp, sample);
+                }
+            }
+        }
+    }
+
+    void checkRandomEvents(HexCell cell, float sample)
+    {
+        if (cell.IsCloud == false)
+        {
+            if (sample < cell.ProbabilityCloud)
+            {
+                createRain(cell);
+            }
+        }
+        else
+        {
+            updateCloudStatus(cell);
+            if (cell.IsRaining == false)
+            {
+                if (sample < cell.ProbabilityRain)
+                {
+                    createRain(cell);
+                }
+            }
+            else
+            {
+                updateRainStatus(cell);
+            }
+        }
+    }
+
+    void updateCloudStatus(HexCell cell)
+    {
+        // write time 
+        /*
+        if (cell.CloudStartTimeStamp + cell.CloudDurationCycles > time)
+        {
+            cell.IsCloud = false;
+        }
+        */
+    }
+    
+    void updateRainStatus(HexCell cell)
+    {
+        //time
+        /*
+        if (cell.RainStartTimeStamp + cell.RainDurationCycles > time)
+        {
+            cell.IsRaining = false;
+        }
+        */
+    }
+
+    void createRain(HexCell cell)
+    {
+        cell.IsRaining = true;
+    }
+
+    void createCloud(HexCell cell)
+    {
+        cell.IsCloud = true;
+    }
+
+    float randomSample()
+    {
+        return Random.Range(0, 1);
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            HexCell currentCell = hexGrid.GetCell(hit.point);
+            if (previousCell && previousCell != currentCell)
+            {
+                ValidateDrag(currentCell);
+            }
+            else
+            {
+                isDrag = false;
+            }
+            EditCells(currentCell);
+            previousCell = currentCell;
+        }
+        else
+        {
+            previousCell = null;
+        }
+    }
+
+    void ValidateDrag(HexCell currentCell)
+    {
+        for (
+            dragDirection = HexDirection.NE;
+            dragDirection <= HexDirection.NW;
+            dragDirection++
+        )
+        {
+            if (previousCell.GetNeighbor(dragDirection) == currentCell)
+            {
+                isDrag = true;
+                return;
+            }
+        }
+        isDrag = false;
+    }
+
+    void EditCells(HexCell center)
+    {
+        int centerX = center.coordinates.X;
+        int centerZ = center.coordinates.Z;
+
+        for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++)
+        {
+            for (int x = centerX - r; x <= centerX + brushSize; x++)
+            {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+        for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++)
+        {
+            for (int x = centerX - brushSize; x <= centerX + r; x++)
+            {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+    }
+
+    void EditCell(HexCell cell)
+    {
+        if (cell)
+        {
+            if (applyColor)
+            {
+                cell.Color = activeColor;
+            }
+            if (applyElevation)
+            {
+                cell.Elevation = activeElevation;
+            }
+            if (applyWaterLevel)
+            {
+                cell.WaterLevel = activeWaterLevel;
+            }
+            if (applyUrbanLevel)
+            {
+                cell.UrbanLevel = activeUrbanLevel;
+                cell.UpdateCell();
+            }
+            if (applyFarmLevel)
+            {
+                cell.FarmLevel = activeFarmLevel;
+                cell.UpdateCell();
+            }
+            if (applyPlantLevel)
+            {
+                cell.PlantLevel = activePlantLevel;
+                cell.UpdateCell();
+            }
+            if (riverMode == OptionalToggle.No)
+            {
+                cell.RemoveRiver();
+            }
+            if (roadMode == OptionalToggle.No)
+            {
+                cell.RemoveRoads();
+            }
+            if (isDrag)
+            {
+                HexCell otherCell = cell.GetNeighbor(dragDirection.Opposite());
+                if (otherCell)
+                {
+                    if (riverMode == OptionalToggle.Yes)
+                    {
+                        otherCell.SetOutgoingRiver(dragDirection);
+                    }
+                    if (roadMode == OptionalToggle.Yes)
+                    {
+                        otherCell.AddRoad(dragDirection);
+                    }
+                }
+            }
+        }
+    }
+}
